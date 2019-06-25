af51cfae766fa9332d5cb1f0a458c42151eaf6f4
ehlerorngard <ehlerorngard@gmail.com>
2019-06-25 16:10:22 -0700

    prod

--- catalog/catalog/settings_development.py
+++ catalog/catalog/settings_development.py
@@ -10,6 +10,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 ALLOWED_HOSTS = ['localhost']
 
